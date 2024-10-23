9bb98700c32b21306dfd92cdd08c3cd10505acaa
Travis Barnette <barnette.travis31@gmail.com>
2024-10-23 09:52:14 -0400

    adding prototype character model

--- app/(game)/index.tsx
+++ app/(game)/index.tsx
@@ -1,56 +1,12 @@
-import React, { useRef } from "react";
-import { View, PanResponder, Text, StyleSheet, Animated } from "react-native";
+import { CharacterModel } from "@/components/characterModel";
+import React from "react";
 
+import { View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function Index() {
-  // Position of the character
-  const pan = useRef(new Animated.ValueXY());
-
-  // PanResponder to handle drag events
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: Animated.event(
-        [
-          null, // Ignore native event
-          { dx: pan.current.x, dy: pan.current.y }, // Map gesture movement to x and y
-        ],
-        { useNativeDriver: false }
-      ),
-      onPanResponderRelease: () => {
-        // Animate back to the original position (0,0)
-        Animated.spring(pan.current, {
-          toValue: { x: 0, y: 0 }, // Snap back to this position
-          useNativeDriver: false, // Required when animating styles in React Native
-        }).start();
-      },
-    })
-  );
-
   return (
-    <View style={styles.container}>
-      {/* Character or any element to be dragged */}
-      <Animated.View
-        style={[pan.current.getLayout(), styles.character]}
-        {...panResponder.current.panHandlers}
-      />
-    </View>
+    <GestureHandlerRootView className="flex-1 justify-center items-center">
+      <CharacterModel />
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  character: {
-    width: 100,
-    height: 100,
-    backgroundColor: "blue",
-    borderRadius: 50,
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.5,
-    shadowRadius: 4,
-  },
-});
